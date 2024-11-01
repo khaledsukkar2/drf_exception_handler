d0d2fae59c8fe3f6ec8f3eb8ec1350eb8f7714e5
Maamoun Haj Najeeb <maamoun.haj.najeeb@gmail.com>
2024-11-01 08:58:23 +0300

    after adding inline exception

--- apps/Example/apis.py
+++ apps/Example/apis.py
@@ -1,8 +1,8 @@
 from django.http import Http404
 from django.core.exceptions import ValidationError, PermissionDenied
 
-from rest_framework import decorators
 from rest_framework.request import Request
+from rest_framework import decorators, status, exceptions
 
 
 @decorators.api_view(http_method_names=["get"])
@@ -18,3 +18,33 @@ def permission_denied(request: Request):
 @decorators.api_view(http_method_names=["get"])
 def not_found(request: Request):
     raise Http404("it's HTTP404")
+
+
+class TooManyRequest(exceptions.APIException):
+    status_code = status.HTTP_429_TOO_MANY_REQUESTS
+    default_code = "Many Request"
+    default_detail = "This resource is closed because you call it too many times"
+
+
+@decorators.api_view(http_method_names=["get"])
+def too_many_request(request: Request):
+    raise TooManyRequest()
+
+
+def create_exception_class(attrs: dict):
+    return type("inline_api_exception", (exceptions.APIException, ), attrs)
+
+def inline_exception(attrs: dict):
+    Exceptionklass = create_exception_class(attrs=attrs)
+    return Exceptionklass
+
+
+@decorators.api_view(http_method_names=["get"])
+def inline_too_many_requests(request: Request):
+    TooManyRequest = inline_exception(dict(
+        status_code=status.HTTP_429_TOO_MANY_REQUESTS,
+        default_code="Many Request",
+        default_detail="This resource is closed because you call it too many times"
+    ))
+
+    raise TooManyRequest
--- apps/Example/urls.py
+++ apps/Example/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("validation_error/", apis.validation_error, name="validation_error"),
     path("permission_denied/", apis.permission_denied, name="permission_denied"),
     path("not_found_error/", apis.not_found, name="not_found"),
+    path("too_many_request/", apis.too_many_request, name="too_many_request"),
+    path("inline_too_many_request/", apis.inline_too_many_requests, name="inline_too_many_request"),
 ]
